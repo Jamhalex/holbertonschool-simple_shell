aab943049e34ccdf8e07f30c2e9f25f4a544dc63
Johnson <jamhalex@gmail.com>
2025-12-17 19:11:36 -0400

    addded lin in shell.h

--- shell_loop.c
+++ shell_loop.c
@@ -26,6 +26,37 @@ static void strip_newline(char *s)
 }
 
 /**
+ * trim_spaces - trims leading and trailing spaces/tabs
+ * @s: string to trim
+ *
+ * Return: pointer to first non-space char (inside s)
+ */
+static char *trim_spaces(char *s)
+{
+	size_t end;
+	char *start;
+
+	if (s == NULL)
+		return (NULL);
+
+	start = s;
+	while (*start == ' ' || *start == '\t')
+		start++;
+
+	if (*start == '\0')
+		return (start);
+
+	end = strlen(start);
+	while (end > 0 && (start[end - 1] == ' ' || start[end - 1] == '\t'))
+	{
+		start[end - 1] = '\0';
+		end--;
+	}
+
+	return (start);
+}
+
+/**
  * run_shell - simple shell loop (no args, no PATH, no builtins)
  * @av0: program name (argv[0]) for error printing
  *
@@ -33,7 +64,7 @@ static void strip_newline(char *s)
  */
 int run_shell(char *av0)
 {
-	char *line;
+	char *line, *cmd;
 	size_t len;
 	ssize_t nread;
 	int interactive;
@@ -55,14 +86,13 @@ int run_shell(char *av0)
 		}
 
 		strip_newline(line);
+		cmd = trim_spaces(line);
 
-		if (line[0] == '\0')
+		if (cmd[0] == '\0')
 			continue;
 
-		if (execute_command(line) == -1)
-		{
+		if (execute_command(cmd) == -1)
 			perror(av0);
-		}
 	}
 }
 
