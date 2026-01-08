46093fea069cd856e6f866feb9bcac75c4e49689
Johnson <jamhalex@gmail.com>
2026-01-08 02:02:10 -0400

    Tier 2: handle SIGINT correctly and document interactive behavior

--- shell_loop.c
+++ shell_loop.c
@@ -1,4 +1,17 @@
 #include "shell.h"
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * sigint_handler - handles Ctrl+C signal in interactive mode
+ * @sig: signal number
+ */
+static void sigint_handler(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n", 1);
+}
 
 /**
  * run_shell - shell entry point
@@ -8,6 +21,9 @@
  */
 int run_shell(char *av0)
 {
+	if (isatty(STDIN_FILENO))
+		signal(SIGINT, sigint_handler);
+
 	return (shell_loop(av0));
 }
 
